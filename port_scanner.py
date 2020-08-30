169b735f24472a3faaf2f6d61cb1d0ab3bf6ba48
Iridium77Ir <65957631+Iridium77Ir@users.noreply.github.com>
2020-08-30 14:46:37 +0200

    removed sleep

--- port_scanner.py
+++ port_scanner.py
@@ -20,10 +20,9 @@ except:
     print('\033[91mMake sure to report this bug to the developer, \033[4m\033[1mwith the traceback!\033[0m\n')
     print_exc()
     input()
-def scanPort(_serverIP, _port, _index):
-    print(f"Scanning port no. \033[32m{_port}\033[0m... \033[34m{loadASCII[_index]}\033[0m")
+def scanPort(_serverIP, _port):
     sock = socket(AF_INET, SOCK_STREAM)  # Creates socket with IPv4 address family and stream type
-    sock.settimeout(0.5)  # Timeout of 5s
+    sock.settimeout(0.1)  # Timeout of 5s
     res = sock.connect_ex((_serverIP, _port))  # Creates connection
     if res == 0:  # If port is open
         openPorts.append(_port)
@@ -42,14 +41,8 @@ def scanServerPorts():
                 else: serverIP = remoteServer
                 port = portMin
                 while port < portMax:
-                    index += 1  # Incrementing index to show loading ascii art
-                    if index == 4:
-                        index = 0
-                    exec("t = Thread(target=scanPort, args=(serverIP, port, index,)); t.start()")  # Creates new thread
+                    exec("t = Thread(target=scanPort, args=(serverIP, port,)); t.start()")  # Creates new thread
                     port += 1
-                    try: sleep(float(speed))
-                    except: sleep(0.005)
-                sys("CLS")
                 for openPort in openPorts:
                     print(f"\033[36m{openPort}\033[0m is open.")  # Logs which ports are open
             except gaierror:  # Host name is invalid
@@ -68,36 +61,6 @@ def scanServerPorts():
 
         host = input("Enter the Host IP/Name: ")
 
-        def speedInput():
-            global speed
-            speed = input("Enter the delay mode (slow, med, fast, ultra, null, custom i.e: '.023'), for help type '?': ")
-            if speed == '?':
-                print("\n\033[1m\033[38;5;208mslow:\033[0m "
-                      + "has a delay of \033[1m0.05\033[0m, not recommended but very accurate.\n"
-                      + "\033[1m\033[38;5;226mmed:\033[0m "
-                      + "has a delay of \033[1m0.02\033[0m, okay but should mainly be used if you have a bad ISP or if you want more accurate results.\n"
-                      + "\033[1m\033[38;5;118mfast:\033[0m "
-                      + "has a delay of \033[1m0.005\033[0m, this is very recommended, as it shouldn't cause much lag but it is faster than usual and somewhat accurate.\n"
-                      + "\033[1m\033[38;5;63multra:\033[0m "
-                      + "has a delay of \033[1m0.003\033[0m, anything more and it might glitch, this should only be used with a good "
-                      + "computer and connection, this isn't very accurate and will likely skip a few open ports.\n"
-                      + "\033[1m\033[38;5;124mnull:\033[0m "
-                      + "has \033[1m\033[38;5;196mno delay\033[0m, highly unrecommended, has a much higher chance of crashing or glitching, if you want to use this"
-                      + ", you might wanna have a super computer\n")
-                speedInput()
-            if str(speed).lower() == "slow":
-                speed = .05
-            elif str(speed).lower() == "med":
-                speed = .02
-            elif str(speed).lower() == "fast":
-                speed = .005
-            elif str(speed).lower() == "ultra":
-                speed = .003
-            elif str(speed).lower() == 'null':
-                speed = 0
-            else:
-                speed = float(speed)
-
         def portInput():
             portNum = input("Enter the range of ports that you want to scan, for help type '?': ")
             if portNum.lower() == '?':
@@ -114,7 +77,6 @@ def scanServerPorts():
                 portMin = int(portNum.split(' ')[0])
                 portMax = int(portNum.split(' ')[1])
 
-        speedInput()
         portInput()
         sys("CLS")
 
